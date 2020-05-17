9911246a366a233fad376e7111b29a577c485807
micnncim <micnncim@gmail.com>
2020-05-17 19:57:36 +0900

    Update commands/git-delete-squash-merged-branch.sh

--- commands/git-delete-squash-merged-branch.sh
+++ commands/git-delete-squash-merged-branch.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copyright 2020 micnncim
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Deletes the squash-merged git branches.
 #
 # This file contains the modified content of https://github.com/not-an-aardvark/git-delete-squashed.
@@ -25,7 +11,7 @@ BLUE="${ESC}[34m"
 CYAN="${ESC}[36m"
 NO_COLOR="${ESC}[m"
 
-git-delete-squash-merged-branches() {
+git-delete-squash-merged-branch() {
   git switch -q master
   branches=$(git for-each-ref refs/heads/ "--format=%(refname:short)" | tr '\n' ' ')
 
@@ -43,21 +29,20 @@ ask() {
   local message input
   message="${1:-Are you sure?}"
 
-  while true
-  do
+  while true; do
     read -r -p "${message} [Y/n] " input
     case "${input}" in
-      Y)
-        return 0
-        ;;
-      n)
-        return 1
-        ;;
-      *)
-        echo "Invalid input... again" >&2
-        ;;
+    Y)
+      return 0
+      ;;
+    n)
+      return 1
+      ;;
+    *)
+      echo "Invalid input... again" >&2
+      ;;
     esac
   done
 }
 
-git-delete-squash-merged-branches
+git-delete-squash-merged-branch
